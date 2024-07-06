7b8e9f3aeacaf751592e9e8770f864a7c511ed70
KnockKnockP <hansu012345@gmail.com>
2024-07-07 07:17:40 +0900

    Fix building.

--- TextEditor.Win32/TextEditor.Win32/main.cpp
+++ TextEditor.Win32/TextEditor.Win32/main.cpp
@@ -1,14 +1,10 @@
-#ifndef UNICODE
-#define UNICODE
-#endif
-
 #include <Windows.h>
 #include <MainWindow.hpp>
 
-int APIENTRY WinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPSTR lpCmdLine,
-                     _In_ int nShowCmd) {
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR lpCmdLine,
+                     int nShowCmd) {
     MainWindow mainWindow;
     mainWindow.Show();
 
