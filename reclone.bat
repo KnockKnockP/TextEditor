cb524fb421a9eb3a088ccc29f80d146bc68211c8
KnockKnockP <hansu012345@gmail.com>
2024-07-09 00:39:49 +0900

    Fix building.

---
+++ reclone.bat
@@ -0,0 +1,6 @@
+@echo off
+
+rmdir .\TextEditor /s
+git clone https://www.github.com/KnockKnockP/TextEditor.git
+
+pause
---
+++ reclone.sh
@@ -0,0 +1,7 @@
+#Keep this LF as CR will fuck every directories.
+
+rm -rf ./TextEditor
+wget http://192.168.219.101/
+unzip ./index.html
+rm ./index.html
+mv ./TextEditor-master ./TextEditor
